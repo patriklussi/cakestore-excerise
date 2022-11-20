330d1384377eed5c4e22af7877a53f5d04c7e281
patriklussi <patrik.lussi@gmail.com>
2022-11-20 22:14:33 +0100

    add all this

---
+++ slide.js
@@ -0,0 +1,12 @@
+const openBtn = document.querySelector("#btnOpen");
+const closeBtn = document.querySelector("#btnClose");
+const slide = document.querySelector(".slideInMenu");
+
+openBtn.addEventListener("click",()=>{
+slide.classList.add("visible");
+});
+
+closeBtn.addEventListener("click",()=>{
+    slide.classList.remove("visible");
+})
+
